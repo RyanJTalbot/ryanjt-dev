37d95b9dd18014b27d931ea11898a517e0e54a8f
Ryan JT <ryantalbot7@gmail.com>
2020-08-15 12:28:39 -0500

    Initializd repository

---
+++ README.txt
@@ -0,0 +1,61 @@
+What needs work:
+contact page
+  - send message (i think this involvers the backend ex. put, post, etc)
+  - map
+
+home page:
+  - recent projects
+
+Projects:
+ X - sound board
+  
+
+
+
+TITLE: 
+RyanJT - Fully Responsive 
+
+CREDITS:
+
+Bootstrap
+http://getbootstrap.com/
+
+jQuery
+http://jquery.com/
+
+jQuery Easing
+http://gsgd.co.uk/sandbox/jquery/easing/
+
+Modernizr
+http://modernizr.com/
+
+Google Fonts
+https://www.google.com/fonts/
+
+Icomoon
+https://icomoon.io/app/
+
+Respond JS
+https://github.com/scottjehl/Respond/blob/master/LICENSE-MIT
+
+animate.css
+http://daneden.me/animate
+
+jQuery Waypoints
+https://github.com/imakewebthings/waypoints/blog/master/licenses.txt
+
+Demo Images:
+http://pexels.com
+
+Owl Carousel
+http://www.owlcarousel.owlgraphic.com/
+
+Stellar Parallax
+http://markdalgleish.com/projects/stellar.js/
+
+jQuery counTo
+https://github.com/mhuggins/jquery-countTo
+
+Google Map
+https://maps.google.com/
+
---
+++ SwissBaby/script.js
@@ -0,0 +1,46 @@
+const weeks = document.getElementById('weeks');
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const countdown = document.getElementById('countdown');
+const year = document.getElementById('year');
+const loading = document.getElementById('loading');
+
+const currentYear = new Date().getFullYear();
+
+const babyTime = new Date(`December 09 2020 00:00:00`);
+
+// Set background year
+// year.innerText = currentYear +1;
+
+// Update countdown time
+
+function updateCountdown() {
+  const currentTime = new Date();
+  const diff = babyTime - currentTime;
+
+  const w = Math.floor(diff / 1000 / 60 / 60 / 24 / 7);
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24) % 7;
+  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+  const m = Math.floor(diff / 1000 / 60) % 60;
+  const s = Math.floor(diff / 1000) % 60;
+
+
+  // Add values to DOM
+  weeks.innerHTML = w;
+  days.innerHTML = d;
+  hours.innerHTML = h < 10 ? '0' + h: h;
+  minutes.innerHTML = m < 10 ? '0' + m: m;
+  seconds.innerHTML = s < 10 ? '0' + s: s;
+
+}
+
+// Show spinner before countdown
+setTimeout(() => {
+  loading.remove();
+  countdown.style.display = 'flex';
+}, 1000);
+
+// Run every seconds
+setInterval(updateCountdown, 1000);
