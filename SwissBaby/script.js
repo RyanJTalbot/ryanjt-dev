cc728bf776ec1b351464c60dd774fdb00620500f
Ryan JT <40918732+RyanJTalbot@users.noreply.github.com>
2020-11-23 09:59:25 -0600

    update hours to be less than 24

--- SwissBaby/script.js
+++ SwissBaby/script.js
@@ -22,7 +22,7 @@ function updateCountdown() {
 
 //   const w = Math.floor(diff / 1000 / 60 / 60 / 24 / 7);
   const d = Math.floor(diff / 1000 / 60 / 60 / 24) ;
-  const h = Math.floor((diff / 1000 / 60 / 60) % 24)+13;
+  const h = Math.floor((diff / 1000 / 60 / 60) % 24);
   const m = Math.floor(diff / 1000 / 60) % 60;
   const s = Math.floor(diff / 1000) % 60;
 
