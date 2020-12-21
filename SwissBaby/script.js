44c49768b25ea973f46d56896e38300e09ae4ce3
Ryan JT <40918732+RyanJTalbot@users.noreply.github.com>
2020-12-20 18:39:06 -0600

    Update script.js

--- SwissBaby/script.js
+++ SwissBaby/script.js
@@ -47,7 +47,7 @@
 
 window.onload = function() {
   // Month Day, Year Hour:Minute:Second, id-of-element-container
-  countUpFromTime("Jan 1, 2014 12:00:00", 'countup1'); // ****** Change this line!
+  countUpFromTime("Dec 4, 2020 00:07:23", 'countup1'); // ****** Change this line!
 };
 function countUpFromTime(countFrom, id) {
   countFrom = new Date(countFrom).getTime();
